491bc2f734723867a19c52c0f1dbca472dc20feb
goujie <1334855187@qq.com>
2021-04-21 21:09:22 +0800

    第二次修改d2

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,22 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h1 v-if="vif">v-if</h1>
+    <h1 v-show="vshow">v-show</h1>
   </div>
 </template>
+<script>
+export default {
+name:'about',
+data () {
+  return {
+    vif:true,
+    vshow:true
+  }
+}
+}
+</script>
+
+<style>
+
+</style>
